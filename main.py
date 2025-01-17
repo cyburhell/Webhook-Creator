a9b21fba82df578b283dd032f041b4b4fe12f777
CyberHell <cyberhell28@gmail.com>
2025-01-17 18:12:40 +0200

    main.py

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 # This code is completely made for Educational Purposes only. 
 # You are responsible for your own actions.
-# Iam not responsible for your actions.
+# I am not responsible for your actions.
 # Use this code at your own risk.
 # Made by DISCORD : arandomguyin2024 | 
-if os.name == 'nt':
- os.system('cls')
-else:
- os.system('clear')
-import requests
-from pystyle import Write, Colors
+
+import os
+try:
+    import requests
+    from pystyle import Write, Colors
+except:
+    os.system("pip install pystyle requests")
+    import requests
+    from pystyle import Write, Colors
+
+    if os.name == 'nt':
+        os.system('cls')
+    else:
+        os.system('clear')
+
 Write.Print("""
  █     █░▓█████  ▄▄▄▄    ██░ ██  ▒█████   ▒█████   ██ ▄█▀    ▄████▄   ██▀███  ▓█████ ▄▄▄     ▄▄▄█████▓ ▒█████   ██▀███  
 ▓█░ █ ░█░▓█   ▀ ▓█████▄ ▓██░ ██▒▒██▒  ██▒▒██▒  ██▒ ██▄█▒    ▒██▀ ▀█  ▓██ ▒ ██▒▓█   ▀▒████▄   ▓  ██▒ ▓▒▒██▒  ██▒▓██ ▒ ██▒
@@ -20,28 +29,30 @@ Write.Print("""
   ░   ░     ░    ░    ░  ░  ░░ ░░ ░ ░ ▒  ░ ░ ░ ▒  ░ ░░ ░    ░          ░░   ░    ░    ░   ▒    ░      ░ ░ ░ ▒    ░░   ░ 
     ░       ░  ░ ░       ░  ░  ░    ░ ░      ░ ░  ░  ░      ░ ░         ░        ░  ░     ░  ░            ░ ░     ░     
                       ░                                     ░                                                           
-""",Colors.red,interval=0)
-TOKEN = Write.Input("Enter your token : ", Colors.blue_to_red,interval=0)
+""", Colors.red, interval=0)
+
+TOKEN = Write.Input("Enter your token : ", Colors.blue_to_red, interval=0)
 channel_id = int(Write.Input("Channel ID : ", Colors.blue_to_cyan, interval=0))
-webhook_name = Write.Input("Enter the webhook name : ", Colors.blue_to_cyan,interval=0)
-numbers = int(Write.Input("How many webhooks do you want to create? : ", Colors.blue_to_cyan, interval=0)) 
+webhook_name = Write.Input("Enter the webhook name : ", Colors.blue_to_cyan, interval=0)
+numbers = int(Write.Input("How many webhooks do you want to create? : ", Colors.blue_to_cyan, interval=0))
+
 url = f"https://discord.com/api/v9/channels/{channel_id}/webhooks"
 name = {
     "name": webhook_name
 }
-headers={
+headers = {
     "Authorization": TOKEN,
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
-    "Content-Type":"application/json"
+    "Content-Type": "application/json"
 }
 
 for i in range(numbers):
-    Write.Print(f"Creating webhook {i+1}...\n", Colors.blue_to_cyan,interval=0)
-    r = requests.post(url, json=name,headers=headers)
+    Write.Print(f"Creating webhook {i+1}...\n", Colors.blue_to_cyan, interval=0)
+    r = requests.post(url, json=name, headers=headers)
 
 if r.status_code == 200:
     response_data = r.json()
     webhook_url = response_data["url"]
-    Write.Print(f"Webhook {i+1} created successfully.", Colors.blue_to_cyan,interval=0)
+    Write.Print(f"Webhook {i+1} created successfully.", Colors.blue_to_cyan, interval=0)
 else:
-    Write.Print(f"Failed to create webhook. Status code: {r.status_code}", Colors.red,interval=0)
+    Write.Print(f"Failed to create webhook. Status code: {r.status_code}", Colors.red, interval=0)
