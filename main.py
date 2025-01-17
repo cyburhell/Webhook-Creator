84c5f4da54a717dcb507bc049644b31423b9f86c
CyberHell <cyberhell28@gmail.com>
2025-01-17 17:31:10 +0200

    main.py

--- main.py
+++ main.py
@@ -18,9 +18,9 @@ Write.Print("""
                       ░                                     ░                                                           
 """,Colors.red,interval=0)
 TOKEN = Write.Input("Enter your token : ", Colors.blue_to_red,interval=0)
-channel_id = int(input("Channel ID : "))
+channel_id = int(Write.Input("Channel ID : ", Colors.blue_to_cyan, interval=0))
 webhook_name = Write.Input("Enter the webhook name : ", Colors.blue_to_cyan,interval=0)
-numbers = int(input("How many webhooks do you want to create ? : "))
+numbers = int(Write.Input("How many webhooks do you want to create? : ", Colors.blue_to_cyan, interval=0)) 
 url = f"https://discord.com/api/v9/channels/{channel_id}/webhooks"
 name = {
     "name": webhook_name
@@ -32,7 +32,7 @@ headers={
 }
 
 for i in range(numbers):
-    print(f"Creating webhook {i+1}...")
+    Write.Print(f"Creating webhook {i+1}...", Colors.blue_to_cyan,interval=0)
     r = requests.post(url, json=name,headers=headers)
 
 if r.status_code == 200:
